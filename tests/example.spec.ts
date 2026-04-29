6eb5838c91ab30febd78e2273bba5c19e97840a7
mehmet-alatas <mehmetqa.techproed@gmail.com>
2026-04-29 22:50:55 +0300

    v2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,6 +7,7 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
+
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
@@ -15,4 +16,17 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+
+test("get started link2", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" }),
+  ).toBeVisible();
 });
